1b2cca95e705b1e300bccb9be14e7457c02b9e0c
Demondude <21774344d4f31b8f16ccd8e12c7de09b>
2019-08-03 11:51:28 +0300

    Update EventManager.py

--- EventManager/EventManager.py
+++ EventManager/EventManager.py
@@ -10,9 +10,24 @@ class EventManager(commands.Cog):
         def user_check(msg):
             return msg.author == ctx.author
 
+        await ctx.send("Enter name of the event.")
+        name_event = await ctx.bot.wait_for_message("message", check=user_check)
         await ctx.send("Enter a quick description of the event.")
         description_event = await ctx.bot.wait_for("message", check=user_check)
-        await ctx.send(description_event.content)
+        await ctx.send("Enter the date of the event example (yyyy/mm/dd).")
+        date_event = await ctx.bot.wait_for("message", check=user_check)
+        await ctx.send("Enter the time of the event example (00:00). The time should be Coordinated Universal Time (UTC).")
+        time_event = await ctx.bot.wait_for("message", check=user_check)
+        
+        embed = discord.Embed(title=name_event, description=description_event, color=0xf2ff00)
+        embed.add_field(name="Time", value=time_event, inline=True)
+        embed.add_field(name="Date", value=date_event, inline=True)
+        embed.set_footer(text="You should get a notification when the event starts.")
+        await ctx.send(embed=embed)
+
+        await ctx.send("It looks like this. Should we post this ? (y/n).")
+        confirmation_event = await ctx.bot.wait_for("message", check=user_check)
+        await ctx.send(confirmation_event.content)
 
 
 def setup(bot):
