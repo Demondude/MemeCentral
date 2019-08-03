add4da1a045f25dd2c36aaac35b37617fabb6b3a
Demondude <21774344d4f31b8f16ccd8e12c7de09b>
2019-08-03 22:23:28 +0300

    Update Hentai.py

--- Hentai/Hentai.py
+++ Hentai/Hentai.py
@@ -14,24 +14,11 @@ def build_embed(url, color):
 class Hentai(commands.Cog):
     @commands.command(autohelp=True, usage="<Hentai tag>")
     async def hentai(self, ctx, tag: str):
-        """All the hentai i can give you."""
-        if tag == "":
-            await ctx.send("""```
-                feet, trap, futanari, hololewd, lewdkemo, solog
-                feetg, cum, erokemo, les, wallpaper, lewdk
-                ngif, meow, tickle, lewd, feed, gecg, eroyuri, eron
-                cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar
-                gasm, poke, anal, slap, hentai, avatar, erofeet, holo
-                keta, blowjob, pussy, tits, holoero, lizard, pussy_jpg
-                pwankg, classic, kuni, waifu, pat, kiss, femdom
-                neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif
-                smallboobs, hug, ero```"""
-                           )
-        else:
-            try:
-                await ctx.send(embed=build_embed(nekos.img(tag), await ctx.embed_color()))
-            except nekos.errors.InvalidArgument:
-                await ctx.send("That tag does not exist. try typing .hentai")
+        """All the hentai i can give you. Here i gave you a list. feet, trap, futanari, hololewd, lewdkemo, solog feetg, cum, erokemo, les, wallpaper, lewdk, ngif, meow, tickle, lewd, feed, gecg, eroyuri, eron, cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar, gasm, poke, anal, slap, hentai, avatar, erofeet, holo ,keta, blowjob, pussy, tits, holoero, lizard, pussy_jpg, pwankg, classic, kuni, waifu, pat, kiss, femdom, neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif, smallboobs, hug, ero```"""
+        try:
+            await ctx.send(embed=build_embed(nekos.img(tag), await ctx.embed_color()))
+        except nekos.errors.InvalidArgument:
+            await ctx.send("That tag does not exist. try typing .hentai")
 
 
 def setup(bot):
