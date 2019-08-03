bb7e2ae6778fd3132bdec985df2b7f22da8f3e5e
Demondude <21774344d4f31b8f16ccd8e12c7de09b>
2019-08-03 22:35:17 +0300

    Update Hentai.py

--- Hentai/Hentai.py
+++ Hentai/Hentai.py
@@ -11,25 +11,30 @@ class Hentai(commands.Cog):
     async def hentai(self, ctx, tag: str):
         """
         All the hentai i can give you. Here i gave you a list.
-        feet, trap, futanari, hololewd, lewdkemo, solog feetg, cum, erokemo,
-        les, wallpaper, lewdk, ngif, meow, tickle, lewd, feed, gecg, eroyuri,
-        eron, cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar, gasm,
-        poke, anal, slap, hentai, avatar, erofeet, holo ,keta, blowjob, pussy,
-        tits, holoero, lizard, pussy_jpg, pwankg, classic, kuni, waifu, pat,
-        kiss, femdom, neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif,
-        smallboobs, hug, ero
+        'feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo',
+        'solog', 'feetg', 'cum', 'erokemo', 'les', 'wallpaper', 'lewdk',
+        'ngif', 'meow', 'tickle', 'lewd', 'feed', 'gecg', 'eroyuri', 'eron',
+        'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'kemonomimi', 'nsfw_avatar',
+        'gasm', 'poke', 'anal', 'slap', 'hentai', 'avatar', 'erofeet', 'holo',
+        'keta', 'blowjob', 'pussy', 'tits', 'holoero', 'lizard', 'pussy_jpg',
+        'pwankg', 'classic', 'kuni', 'waifu', 'pat', 'kiss', 'femdom', 'neko',
+        'spank', 'cuddle', 'erok', 'fox_girl', 'boobs', 'random_hentai_gif',
+        'smallboobs', 'hug', 'ero'
         """
-        try:
-            url = nekos.img(tag)
-        except nekos.errors.InvalidArgument:
-            await ctx.send("That tag does not exist. try typing .hentai")
+        if ctx.channel.is_nsfw():
+            ctx.send("Wrong channel. Go to nsfw")
+        else:
+            try:
+                url = nekos.img(tag)
+            except nekos.errors.InvalidArgument:
+                await ctx.send("That tag does not exist. try typing .hentai")
 
-        embed = discord.Embed(
-            color=await ctx.embed_color()
-        )
-        embed.set_image(url=url)
-        embed.set_footer(text="Thanks Nekos-life!")
-        await ctx.send(embed=embed)
+            embed = discord.Embed(
+                color=await ctx.embed_color()
+            )
+            embed.set_image(url=url)
+            embed.set_footer(text="Thanks Nekos-life!")
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Hentai(bot))
