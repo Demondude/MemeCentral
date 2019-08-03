129123e5a6fca7fa343b8f649d87f20ea9784cea
Demondude <21774344d4f31b8f16ccd8e12c7de09b>
2019-08-03 22:16:40 +0300

    Update Hentai.py

--- Hentai/Hentai.py
+++ Hentai/Hentai.py
@@ -2,6 +2,7 @@ import discord
 import nekos
 from redbot.core import commands
 
+
 def build_embed(url, color):
     embed = discord.Embed(
         color=color
@@ -9,26 +10,29 @@ def build_embed(url, color):
     embed.set_image(url=url)
     embed.set_footer(text="Thanks Nekos-life!")
 
+
 class Hentai(commands.Cog):
-    @commands.group(autohelp=True)
-    async def hentai(self):
+    @commands.command(autohelp=True, usage="<Hentai tag>")
+    async def hentai(self, ctx, tag: str):
         """All the hentai i can give you."""
-        pass
+        if tag == "":
+            await ctx.send("""```
+                feet, trap, futanari, hololewd, lewdkemo, solog
+                feetg, cum, erokemo, les, wallpaper, lewdk
+                ngif, meow, tickle, lewd, feed, gecg, eroyuri, eron
+                cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar
+                gasm, poke, anal, slap, hentai, avatar, erofeet, holo
+                keta, blowjob, pussy, tits, holoero, lizard, pussy_jpg
+                pwankg, classic, kuni, waifu, pat, kiss, femdom
+                neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif
+                smallboobs, hug, ero```"""
+                           )
+        else:
+            try:
+                await ctx.send(embed=build_embed(nekos.img(tag), await ctx.embed_color()))
+            except nekos.errors.InvalidArgument:
+                await ctx.send("That tag does not exist. try typing .hentai")
 
-    @hentai.command(name="feet")
-    async def feet(self, ctx):
-        """Feet pictures."""
-        await ctx.send(embed=build_embed(nekos.img("feet"), await ctx.embed_color()))
-
-    @hentai.command(name="yuri")
-    async def yuri(self, ctx):
-        """Yuri pictures."""
-        await ctx.send(embed=build_embed(nekos.img("yuri"), await ctx.embed_color()))
-
-    @hentai.command(name="trap")
-    async def trap(self, ctx):
-        """trap pictures."""
-        await ctx.send(embed=build_embed(nekos.img("trap"), await ctx.embed_color()))
 
 def setup(bot):
     bot.add_cog(Hentai(bot))
