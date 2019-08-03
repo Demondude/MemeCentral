49dad890850816912301f617110f496db1cfe383
Demondude <21774344d4f31b8f16ccd8e12c7de09b>
2019-08-03 22:25:59 +0300

    Final v1

--- Hentai/Hentai.py
+++ Hentai/Hentai.py
@@ -9,12 +9,22 @@ def build_embed(url, color):
     )
     embed.set_image(url=url)
     embed.set_footer(text="Thanks Nekos-life!")
+    return embed
 
 
 class Hentai(commands.Cog):
     @commands.command(autohelp=True, usage="<Hentai tag>")
     async def hentai(self, ctx, tag: str):
-        """All the hentai i can give you. Here i gave you a list. feet, trap, futanari, hololewd, lewdkemo, solog feetg, cum, erokemo, les, wallpaper, lewdk, ngif, meow, tickle, lewd, feed, gecg, eroyuri, eron, cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar, gasm, poke, anal, slap, hentai, avatar, erofeet, holo ,keta, blowjob, pussy, tits, holoero, lizard, pussy_jpg, pwankg, classic, kuni, waifu, pat, kiss, femdom, neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif, smallboobs, hug, ero```"""
+        """
+        All the hentai i can give you. Here i gave you a list.
+        feet, trap, futanari, hololewd, lewdkemo, solog feetg, cum, erokemo,
+        les, wallpaper, lewdk, ngif, meow, tickle, lewd, feed, gecg, eroyuri,
+        eron, cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar, gasm,
+        poke, anal, slap, hentai, avatar, erofeet, holo ,keta, blowjob, pussy,
+        tits, holoero, lizard, pussy_jpg, pwankg, classic, kuni, waifu, pat,
+        kiss, femdom, neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif,
+        smallboobs, hug, ero
+        """
         try:
             await ctx.send(embed=build_embed(nekos.img(tag), await ctx.embed_color()))
         except nekos.errors.InvalidArgument:
